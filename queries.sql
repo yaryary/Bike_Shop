7ca4b08588f012abc3ae0a892d39964d8eca4904
Lorena Vazquez <157767532+lorenavazq18@users.noreply.github.com>
2026-04-10 21:44:30 -0400

    Added another Bike query

--- queries.sql
+++ queries.sql
@@ -8,33 +8,44 @@ SELECT bike_status, COUNT(*) AS total_bikes
 FROM Bike
 GROUP BY bike_status;
 
---Query 3: View all repairs
+--Query 3 - counts bikes by average price and bike's type
+SELECT 
+    bike_type,
+    bike_status,
+    COUNT(*) AS total_bikes,
+    AVG(bike_price) AS average_price,
+    MAX(bike_price) AS highest_price
+FROM Bike
+GROUP BY bike_type, bike_status
+ORDER BY average_price DESC;
+
+--Query 4: View all repairs
 SELECT bike_repair_ID, bike_ID, repair_description,
        ticket_creation, ticket_completed, parts_costs, labor_cost
 FROM BikeRepairs;
 
---query 4: repairs per bike.'
+--query 5: repairs per bike.'
 SELECT bike_ID, COUNT(bike_repair_ID) AS total_repairs
 FROM BikeRepairs
 GROUP BY bike_ID;
 
---Query 5: complete repairs
+--Query 6: complete repairs
 SELECT * FROM CompletedRepairs;
 
---Query6: insert repair, put values such as the Bike_ID and the number assigned to it
+--Query7: insert repair, put values such as the Bike_ID and the number assigned to it
 INSERT INTO BikeRepairs(--bike_ID)
 VALUES(--3)
 
---Query 7: for ongoing repairs
+--Query 8: for ongoing repairs
 SELECT bike_repair_ID, bike_ID, repair_description
 FROM BikeRepairs
 WHERE ticket_completed IS NULL;
 
---Query 8: for deleting a repair
+--Query 9: for deleting a repair
 DELETE FROM BikeRepairs
 WHERE bike_repair_ID = --EX: 6;
 
---Query 9: updating a repair
+--Query 10: updating a repair
 UPDATE BikeRepairs
 SET ticket_completed = CURDATE()
 WHERE bike_repair_ID = --Ex: 3;
